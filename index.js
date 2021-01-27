123f479954d11799da3b4bcb1ecc537556db2bb2
ThePrakharRajpali <rprakhar@iitg.ac.in>
2021-01-28 01:52:27 +0530

    sending messages with UDP

--- index.js
+++ index.js
@@ -2,5 +2,26 @@
 const fs = require('fs');
 const bencode = require('bencode')
 
+// 1
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.toString('utf8'));
+
+// 2
+const url = urlParse(torrent.announce.toString('utf8'));
+
+//3
+const socket = dgram.createSocket('udp4');
+
+//4
+const myMsg = Buffer.from('hello?', 'utf8');
+
+//5
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+//6
+socket.on('message', msg => {
+    console.log('message ls', msg);
+});
